4bbc059ed7e419a31a264f434aaef837b87b1510
martinvidela <videla116@gmail.com>
2025-07-31 12:05:30 -0300

    feat: prueba

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import { Zap } from "lucide-react";
+import logo from "../../src/assets/u-sola.png";
 
 const Footer = () => {
   return (
@@ -6,13 +7,15 @@ const Footer = () => {
       <div className="container mx-auto px-4 py-8">
         <div className="flex flex-col md:flex-row items-center justify-between">
           <div className="flex items-center space-x-2 mb-4 md:mb-0">
-            <div className="p-2 bg-gradient-primary rounded-lg">
-              <Zap className="h-5 w-5 text-primary-foreground" />
+            <div className="p-2 bg-gradient-primaryasdasd rounded-lg">
+              <img src={logo} className="w-8" alt="" />
             </div>
             <span className="text-lg font-bold text-foreground">UltraCore</span>
-            <span className="text-sm text-tech-blue font-medium">Soluciones IT</span>
+            <span className="text-sm text-tech-blue font-medium">
+              Soluciones IT
+            </span>
           </div>
-          
+
           <div className="text-center md:text-right">
             <p className="text-muted-foreground text-sm">
               © 2024 UltraCore Soluciones IT. Todos los derechos reservados.
